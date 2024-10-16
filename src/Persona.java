09c3f9098dc16a7d13bf8163a6da989400a54fdd
profefzcesde <profefzcesde@gmail.com>
2024-10-16 10:02:58 -0500

    Revision 1

---
+++ src/Persona.java
@@ -0,0 +1,12 @@
+public class Persona {
+    private String fullname;
+    private String phone;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+}
